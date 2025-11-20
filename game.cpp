fac071296a5fdb4143e180d33701ac42720fbaf4
CorentinVyn <167628431+CorentinVyn@users.noreply.github.com>
2025-11-20 15:26:58 +0100

    ajout game cpp

--- game.cpp
+++ game.cpp
@@ -1,1 +1,38 @@
-c'est le jeu
+#include "game.h"
+#include <iostream>
+using namespace std;
+
+Game::Game() : j1()
+{
+    currentPlayer = &j1;
+}
+
+void Game::selectNames()
+{
+    cout << "Veuillez saisir votre nom : ";
+    string name1;
+    cin >> name1;
+    j1.setNom(name1);
+}
+
+void Game::start()
+{
+    int tour = 1;
+    int score1= 0;
+    int score2= 0;
+    while (true)
+    {
+        cout << "Bienvenue dans ce Pierre-Feuille-Ciseaux, " << j1.getNom() << "!" << endl;
+        // Logique du jeu à implémenter ici
+        cout << "Tour n° " << tour << " : " << endl;
+        if score1 >= 3 {
+            cout << j1.getNom() << " a gagne la partie!" << endl;
+            break;
+        }
+        if score2 >= 3 {
+            cout << "L'IA a gagne la partie!" << endl;
+            break;
+        }
+        tour++;
+    }
+}
