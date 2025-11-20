ee5a1bd694ab836b02b03d0a359dc39ce2308e28
CorentinVyn <167628431+CorentinVyn@users.noreply.github.com>
2025-11-20 14:43:32 +0100

    Ajout des fichiers joueur

---
+++ joueur.h
@@ -0,0 +1,14 @@
+#ifndef JOUEUR_H
+#define JOUEUR_H
+#include <string>
+using namespace std;
+
+class Joueur {
+    private :
+        string nom;
+    public:
+        Joueur();
+        Joueur(string, n);
+        string getNom() const;
+        void setNom(const string& value);
+};
