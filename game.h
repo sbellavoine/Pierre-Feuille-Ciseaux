6ee75dc1e616a83215ee942e687e93f3ca295aec
CorentinVyn <167628431+CorentinVyn@users.noreply.github.com>
2025-11-20 15:17:28 +0100

    ajout du fichier game pour la gestion global

---
+++ game.h
@@ -0,0 +1,16 @@
+#ifndef GAME_H
+#define GAME_H
+#include "Joueur.h"
+
+class Game{
+    private:
+        Joueur j1;
+        Joueur* currentPlayer;
+
+    public:
+        Game();
+        void start();
+        void selectNames();
+}
+
+#endif
